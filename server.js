cce3920c584e4a842361fc498f5a9b040e986bb9
Namit Chandwani <namitchandwani99@gmail.com>
2020-07-11 21:33:03 +0530

    Minor fixes

--- server.js
+++ server.js
@@ -2,30 +2,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
 
 const users = require("./routes/api/users");
+const reports = require("./routes/api/reports");
+const finger = require("./routes/api/finger");
+const xray = require("./routes/api/xray");
 
 const app = express();
 
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
 );
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = process.env.MONGO_URI;
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -35,7 +38,14 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+app.use("/api/reports", reports);
+app.use("/api/finger", finger);
+app.use("/api/xray", xray);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.listen(port, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${port} !`
+  )
+);
