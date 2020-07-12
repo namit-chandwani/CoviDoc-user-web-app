8286b0582c5a637b4f2f90dd180a0f20fe4e8783
Namit Chandwani <namitchandwani99@gmail.com>
2020-07-12 17:45:54 +0530

    Add dummy card for initialization

--- client/src/components/reports/Reports.js
+++ client/src/components/reports/Reports.js
@@ -3,8 +3,45 @@ import React, { Component } from "react";
 export class Reports extends Component {
   render() {
     return (
-      <div>
-        <h1>Reports Component</h1>
+      <div className="row">
+        <div className="col s12 m6">
+          <div
+            className="card #ffd54f amber lighten-2"
+            style={{
+              marginLeft: "200px",
+              width: "700px",
+            }}
+          >
+            <div className="card-content gray-text">
+              <span className="card-title">Report Score:</span>
+              <p
+                className="btn-large"
+                style={{
+                  marginLeft: "200px",
+                  fontWeight: "bold",
+                  fontSize: "2em",
+                  color: "yellow",
+                  cursor: "default",
+                }}
+              >
+                48
+              </p>
+            </div>
+            <div className="card-action">
+              Date created:
+              <span
+                style={{
+                  marginLeft: "200px",
+                  fontWeight: "bold",
+                  fontSize: "1.2em",
+                  color: "gray",
+                }}
+              >
+                23/11/2020
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
