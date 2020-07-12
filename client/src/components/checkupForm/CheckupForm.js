e9ae9f4e15ed5844d1166f02f228d4a609285bb3
Namit Chandwani <namitchandwani99@gmail.com>
2020-07-12 05:36:11 +0530

    Initialize Dashboard child components

---
+++ client/src/components/checkupForm/CheckupForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class CheckupForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>CheckupForm Component</h1>
+      </div>
+    );
+  }
+}
+
+export default CheckupForm;
---
+++ client/src/components/reports/Reports.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Reports extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Reports Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Reports;
