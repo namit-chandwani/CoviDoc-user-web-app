2ccc962b6f9bd53e11e86f550bc79631fcb61b31
Namit Chandwani <namitchandwani99@gmail.com>
2020-07-12 20:33:41 +0530

    Update report schema

--- models/Report.js
+++ models/Report.js
@@ -3,52 +3,8 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const ReportSchema = new Schema({
-  age: {
+  score: {
     type: Number,
-    required: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-  },
-  postalCode: {
-    type: Number,
-    required: true,
-  },
-  existingConditions: {
-    type: [String],
-    required: true,
-  },
-  intTravel: {
-    type: String,
-    required: true,
-  },
-  hotspotZone: {
-    type: String,
-    required: true,
-  },
-  aroundCovidPatient: {
-    type: String,
-    required: true,
-  },
-  liveFacility: {
-    type: String,
-    required: true,
-  },
-  carAccess: {
-    type: String,
-    required: true,
-  },
-  symptoms: {
-    type: [String],
-    required: true,
-  },
-  videoResult: {
-    type: String,
-    required: true,
-  },
-  xrayResult: {
-    type: String,
     required: true,
   },
   user: {
